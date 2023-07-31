f5481fa4e55c5a29c9ec237d57dbf2724a3b4a6f
VictorVariety <victorv@getacademy.no>
2023-07-31 13:45:36 +0200

    update

---
+++ src/app/components/LeftSideBar.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { BiHomeCircle, BiUser } from "react-icons/bi";
+import {
+  BsBell,
+  BsBookmark,
+  BsTwitter,
+  BsEnvelope,
+  BsThreeDots,
+} from "react-icons/bs";
+import { HiOutlineHashtag } from "react-icons/hi";
+
+const NAVIGATION_ITEMS = [
+  {
+    title: "Twitter",
+    icon: BsTwitter,
+  },
+  {
+    title: "Home",
+    icon: BiHomeCircle,
+  },
+  {
+    title: "Explore",
+    icon: HiOutlineHashtag,
+  },
+  {
+    title: "Notifications",
+    icon: BsBell,
+  },
+  {
+    title: "Messages",
+    icon: BsEnvelope,
+  },
+  {
+    title: "Bookmarks",
+    icon: BsBookmark,
+  },
+  {
+    title: "Profile",
+    icon: BiUser,
+  },
+];
+
+const LeftSideBar = () => {
+  return (
+    <section className="fixed w-{275px} flex flex-col items-stretch h-screen">
+      <div className="flex flex-col items-stretch h-full space-y-4 mt-4">
+        {NAVIGATION_ITEMS.map((item) => (
+          <a
+            className="text-2xl rounded-3xl py-2 px-6 space-x-4 flex items-center justify-start w-fit
+        hover:bg-white/20 transition duration-200"
+            href={`/${item.title.toLowerCase()}`}
+            key={item.title}
+          >
+            <div>
+              <item.icon />
+            </div>
+            {item.title !== "Twitter" && <div>{item.title}</div>}
+          </a>
+        ))}
+        <button className="rounded-full bg-primary p-4 m-6 text-center text-2xl hover:bg-opacity-70 transition duration-200">
+          Tweet
+        </button>
+      </div>
+
+      <button className="rounded-full flex items-center space-x-2 bg-transparent w-full justify-between p-4 m-6 text-center hover:bg-white/20 transition duration-200">
+        <div className="flex items-center space-x-2">
+          <div className="rounded-full bg-slate-400 w-10 h-10 "></div>
+          <div className="text-left text-xs">
+            <div className="font-semibold">ProfileName</div>
+            <div>@UserName</div>
+          </div>
+        </div>
+        <div>
+          <BsThreeDots />
+        </div>
+      </button>
+    </section>
+  );
+};
+
+export default LeftSideBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,110 @@
-import Image from 'next/image'
+import { BsDot, BsChat, BsThreeDots } from "react-icons/bs";
+import { IoStatsChart, IoShareOutline } from "react-icons/io5";
+import {
+  AiOutlineLike,
+  AiOutlineRetweet,
+  AiOutlineHeart,
+} from "react-icons/ai";
+import LeftSideBar from "./components/LeftSideBar";
 
-export default function Home() {
+const page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="w-full h-full flex justify-center items-center relative bg-black">
+      <div className="max-w-screen-xl w-full h-full flex relative">
+        <LeftSideBar />
+        <main
+          className="
+          flex flex-col 
+          ml-[275px] w-[600px] h-full min-h-screen
+          border-l border-r border-gray-700"
+        >
+          <h1 className="text-2xl font-bold p-6 backdrop-blur bg-black/10 sticky top-0">
+            Home
+          </h1>
+          <div className="p-4 relative flex items-stretch space-x-2 border-t border-b border-gray-700">
+            <div className="w-10 h-10 bg-slate-200 rounded-full flex-none"></div>
+            <div className="flex flex-col w-full">
+              <input
+                type="text"
+                placeholder="What's up?"
+                className="w-full h-full bg-transparent p-4
+                border-none outline-none border-b-[0.5px] border-gray-700
+                text-xl text-gray-400"
+              />
+              <div className="w-full justify-between items-center flex">
+                <div className=""></div>
+                <div className="w-full max-w-[100px]">
+                  <button
+                    className="px-4 py-2 w-full rounded-full
+                  text-center text-lg font-bold bg-primary
+                  hover:bg-opacity-70 transition duration-200"
+                  >
+                    Tweet
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <div
+                key={i}
+                className="border-b border-gray-700 flex space-x-4 p-4 pb-2"
+              >
+                <div>
+                  <div className="w-10 h-10 bg-slate-200 rounded-full"></div>
+                </div>
+                <div className="flex flex-col">
+                  <div className="flex items-center my-1 text-gray-500 justify-between">
+                    <div className="flex items-center space-x-1 ">
+                      <div className="font-bold text-white">Name</div>
+                      <div>@Username</div>
+                      <div>
+                        <BsDot />
+                      </div>
+                      <div>1 hour ago</div>
+                    </div>
+                    <div className="cursor-pointer text-white">
+                      <BsThreeDots />
+                    </div>
+                  </div>
+                  <div className="text-white text-base">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+                    sequi eaque at sint rem dolorum sit possimus, cum facere.
+                    Voluptatem sapiente recusandae facere accusamus fugit, nisi
+                    in dignissimos alias accusantium? Nemo repellendus quia
+                    nulla modi dolor, officia hic officiis voluptatibus minima
+                    harum, enim distinctio. Id culpa sapiente unde quis
+                    reiciendis.
+                  </div>
+                  <div className="w-full h-96 rounded-xl aspect-square bg-slate-400 mt-2"></div>
+                  <div className="flex items-center justify-around w-full mt-2">
+                    <div className="rounded-full p-3 hover:bg-white/20 transition duration-200 cursor-pointer">
+                      <BsChat />
+                    </div>
+                    <div className="rounded-full p-3 hover:bg-white/20 transition duration-200 cursor-pointer">
+                      <AiOutlineRetweet />
+                    </div>
+                    <div className="rounded-full p-3 hover:bg-white/20 transition duration-200 cursor-pointer">
+                      <AiOutlineHeart />
+                    </div>
+                    <div className="rounded-full p-3 hover:bg-white/20 transition duration-200 cursor-pointer">
+                      <IoStatsChart />
+                    </div>
+                    <div className="rounded-full p-3 hover:bg-white/20 transition duration-200 cursor-pointer">
+                      <IoShareOutline />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
+        {/*         <div>home feed</div>
+        <section>right section</section> */}
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default page;
